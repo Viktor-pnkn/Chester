93e5fb6b860d64acd96f97c6e5d530ed5034b6a4
Viktor-pnkn <tommegan99@gmail.com>
2021-08-10 06:52:28 +0300

    Initial commit

---
+++ src/test/java/ru/gbf/chester/ChesterApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gbf.chester;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChesterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
